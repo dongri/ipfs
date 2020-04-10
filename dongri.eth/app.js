b644e28ea9b38dfc0363a132eaf54239dbf4074e
Dongri Jin <dongrify@gmail.com>
2020-04-10 17:46:37 +0900

    auto commit

--- dongri.eth/app.js
+++ dongri.eth/app.js
@@ -1,3 +1,9 @@
+var url = location.href
+if(url.indexOf('http:') >= 0) {
+  var newUrl = url.replace('http:', 'https:');
+  location.replace(newUrl);
+}
+
 new Vue({
   el: '#year',
   data: { message: ' © ' + new Date().getFullYear() }
