c0ffbeb72a2d2702bab19fea0a4c136aeba9d886
Dongri Jin <dongrify@gmail.com>
2022-11-27 11:18:19 +0900

    Add theme

--- dongri.eth/app.js
+++ dongri.eth/app.js
@@ -8,3 +8,33 @@ new Vue({
   el: '#year',
   data: { message: ' © ' + new Date().getFullYear() }
 })
+
+const setTheme = (theme) => {
+  if (theme === 'dark') {
+    document.body.classList.add('dark')
+    document.getElementById('theme').innerHTML = "🌙"
+  } else if (theme === 'light') {
+    document.body.classList.remove('dark')
+    document.getElementById('theme').innerHTML = "🌞"
+  } else {
+    document.body.classList.remove('dark')
+    document.getElementById('theme').innerHTML = "🧐"
+  }
+}
+
+const userTheme = localStorage.getItem('theme')
+if (userTheme) {
+  setTheme(userTheme)
+} else {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    setTheme('dark')
+  } else {
+    setTheme('light')
+  }
+}
+
+document.getElementById('theme').addEventListener('click', function() {
+  const theme = document.body.classList.contains('dark') ? 'light' : 'dark'
+  setTheme(theme)
+  localStorage.setItem('theme', theme)
+})
